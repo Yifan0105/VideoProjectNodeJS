500f8fb980e7c059e13c1b8dd0c5bfff56b27787
yifan0105 <yifanshi0105@gmail.com>
2025-02-17 16:11:26 +0100

    updata

--- http-server/server.js
+++ http-server/server.js
@@ -1,10 +1,16 @@
 // import http module\
 
-var http = require('http')
+var http = require("http");
 
 // creat server
 
-var server = http.createServer()
-server.listen(8080,function(){
-    console.log(' http://localhost:8080/');
-})
+var server = http.createServer();
+server.listen(8080, function () {
+  console.log(" http://localhost:8080/");
+});
+
+server.on("request", function (req, res) {
+  console.log("Received request");
+  res.write("你好");
+  res.end();
+});
